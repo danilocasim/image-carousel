9e0cc74035be68d9f91ce0e274e34563d4c81e99
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-02-13 10:34:20 +0800

    Add listener for right and left button and set interval to slide using margin

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,39 @@
 import style from "../css/style.css";
+
+const left = document.querySelector("#left");
+const right = document.querySelector("#right");
+
+let px = 0;
+
+left.addEventListener("click", () => {
+  const slides = document.querySelector("#slides");
+
+  if (px === -600) {
+    px = 0;
+  }
+
+  slides.style.marginLeft = px + "px";
+  px -= 150;
+});
+
+right.addEventListener("click", () => {
+  const slides = document.querySelector("#slides");
+
+  if (px === 0) {
+    px = -600;
+  }
+  px += 150;
+
+  slides.style.marginLeft = px + "px";
+});
+
+setInterval(() => {
+  const slides = document.querySelector("#slides");
+
+  if (px === -600) {
+    px = 0;
+  }
+
+  slides.style.marginLeft = px + "px";
+  px -= 150;
+}, 5000);
