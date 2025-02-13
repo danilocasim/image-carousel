4e05a4e4e4e974f617d4e3b25330c36619e679be
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-02-13 13:46:22 +0800

    Add the render functionality and shifting element in array change image

--- src/js/index.js
+++ src/js/index.js
@@ -1,39 +1,45 @@
 import style from "../css/style.css";
+import image1 from "../assets/image1.jpg";
+import image2 from "../assets/image2.jpg";
+import image3 from "../assets/image3.jpg";
+import image4 from "../assets/image4.jpg";
+
+const images = [image1, image2, image4, image3, image4];
+
+function render() {
+  const wrapper = document.querySelectorAll(".slide");
+  wrapper.forEach((img) => {
+    img.remove();
+  });
+
+  const slides = document.querySelector(".slides");
+  images.forEach((img, index) => {
+    if (index === 0) {
+      const imageWrapper = document.createElement("div");
+      imageWrapper.classList.add("slide");
+      const image = document.createElement("img");
+      image.src = img;
+
+      imageWrapper.appendChild(image);
+      slides.appendChild(imageWrapper);
+    }
+  });
+}
+
+render();
 
 const left = document.querySelector("#left");
+left.addEventListener("click", () => {
+  const firstImg = images.shift();
+  images.push(firstImg);
+
+  render();
+});
+
 const right = document.querySelector("#right");
 
-let px = 0;
-
-left.addEventListener("click", () => {
-  const slides = document.querySelector("#slides");
-
-  if (px === -600) {
-    px = 0;
-  }
-
-  slides.style.marginLeft = px + "px";
-  px -= 150;
-});
-
 right.addEventListener("click", () => {
-  const slides = document.querySelector("#slides");
-
-  if (px === 0) {
-    px = -600;
-  }
-  px += 150;
-
-  slides.style.marginLeft = px + "px";
+  const lastImg = images.pop();
+  images.unshift(lastImg);
+  render();
 });
-
-setInterval(() => {
-  const slides = document.querySelector("#slides");
-
-  if (px === -600) {
-    px = 0;
-  }
-
-  slides.style.marginLeft = px + "px";
-  px -= 150;
-}, 5000);
